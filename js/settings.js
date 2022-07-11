63456fba4e6ffa9417cbbbc49d7b6398051db9d2
zen <designerzen@gmail.com>
2022-07-11 13:56:14 +0100

    update: settings comments

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,15 @@
-
 // Settings!
-export const TIME_BETWEEN_BOLTS = 400
+
+
+// All the onscreeb bolt answers to also act as buttons for bolt selection
 export const ALLOW_ONSCREEN_BOLTS_TO_SELECT = false
+
+// Time out - in ms - how long before we consider the user has left due to inactivity?
 export const TIME_DURATION_BEFORE_WE_CONSIDER_USER_LEFT = 1 * 60 * 1000 
+
+
+// Do we want a bolt to be automatically selected (no magnet necessary)
 export const AUTOMATICALLY_SELECT_NEXT_BOLT = true
+
+// and if we do want that behaviour - how long do we wait before selecting it?
+export const TIME_BETWEEN_BOLTS = 400
